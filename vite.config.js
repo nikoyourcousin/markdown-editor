f35f54721db7fb8cca5b5d445ad8943d0dbbee60
nikoyourcousin <118354950+nikoyourcousin@users.noreply.github.com>
2026-04-09 17:25:17 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/markdown-editor/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
